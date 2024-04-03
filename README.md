ce441d31f6e39c9577a14ec05a035a473a076a84
pprog <29218920+pprogrammingg@users.noreply.github.com>
2024-04-03 14:17:02 -0400

    Go REST App setup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # golang_services
 API services written in GoLang
+
+# Table of Contents
+
+1. [Service List](#services)
+
+
+# Services
---
+++ setenv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Set environment variables
+export APP_DB_USERNAME=postgres
+export APP_DB_PASSWORD=
+export APP_DB_NAME=postgres

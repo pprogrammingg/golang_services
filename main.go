84da36c414a18d706249d1fb1a5727fecc2b4a88
pprog <29218920+pprogrammingg@users.noreply.github.com>
2024-04-09 23:25:18 -0400

    try keep server alive with cron job

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	go startCronJob()
 
 	server := NewAPIServer(":8080")
 	server.Run()
+}
+
+func startCronJob() {
+	fmt.Println("Starting Cron job to keep pinging!")
+
+	// Define a ticker that ticks every 6 min
+	ticker := time.NewTicker(360 * time.Second)
+
+	// Start an infinite loop to perform the task periodically
+	for {
+		select {
+		case <-ticker.C:
+			// Ping the health endpoint
+			fmt.Printf("Cron ping - keeping it alive!")
+		}
+	}
 }
